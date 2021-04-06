a4594db8e27cf359f8fb1374706945fdbb1d3d18
Ryensu <ryensu@qq.com>
2021-04-06 18:45:04 +0800

    [新增]音频列表

--- app/src/main/java/com/su/sharkmanfunc/SettingsFragmentCompat.kt
+++ app/src/main/java/com/su/sharkmanfunc/SettingsFragmentCompat.kt
@@ -1,17 +1,17 @@
 package com.su.sharkmanfunc
 
+import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
-import androidx.preference.Preference
-import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.SeekBarPreference
-import androidx.preference.SwitchPreference
+import androidx.preference.*
 
 class SettingsFragmentCompat : PreferenceFragmentCompat() {
 
     companion object {
         var isEnableAudio = false
+        private const val SOUNDS_PATH = "sounds"
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
@@ -41,6 +41,68 @@ class SettingsFragmentCompat : PreferenceFragmentCompat() {
                         isEnableAudio = it.isChecked
                     true
                 }
+        }
+
+        initSounds()
+    }
+
+    private fun initSounds() {
+        findPreference<PreferenceCategory>(getString(R.string.charge_sounds))?.apply {
+            try {
+                val am = context.assets
+                val list = am.list(SOUNDS_PATH)
+                var index = 1
+                list?.forEach {
+                    Log.d("文件", it)
+                    val pre = SoundPreference(requireContext(), "${SOUNDS_PATH}/$it").apply {
+                        title = "音频${index++}"
+                        isIconSpaceReserved = false
+                    }
+                    addPreference(pre)
+                }
+            } catch (e: Exception) {
+
+            }
+        }
+    }
+
+    private fun traversAssets(context: Context, path: String) {
+        val am = context.assets
+
+    }
+
+    class SoundPreference(context: Context, soundPath: String) : Preference(context) {
+
+        companion object {
+            private var media: MediaPlayer? = null
+
+            fun releaseMedia() {
+                media?.apply {
+                    stop()
+                    release()
+                    media = null
+                }
+            }
+        }
+
+        init {
+            if (media == null)
+                media = MediaPlayer().apply {
+                    setOnPreparedListener {
+                        start()
+                    }
+                }
+            setOnPreferenceClickListener {
+                media?.apply {
+                    Log.d("播放", soundPath)
+                    val am = context.assets.openFd(soundPath)
+                    stop()
+                    reset()
+                    setDataSource(am.fileDescriptor, am.startOffset, am.length)
+                    prepareAsync()
+                }
+                true
+            }
         }
     }
 
