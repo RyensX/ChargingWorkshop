67b617727e5f313a8b656e446d09dfd8172231d2
Ryensu <ryensu@qq.com>
2021-04-06 23:06:32 +0800

    [新增]全局异常捕捉

---
+++ app/src/main/java/com/su/sharkmanfunc/App.kt
@@ -0,0 +1,19 @@
+package com.su.sharkmanfunc
+
+import android.app.Application
+import com.su.appcrashhandler.AppCatchException
+import com.su.appcrashhandler.CrashActivityImpl
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        //初始化全局异常处理
+        CrashActivityImpl.apply {
+            KUAN_URL = "https://www.bilibili.com/video/BV1iA41157NL/"
+        }
+        AppCatchException.bindCrashHandler(this) {
+            !BuildConfig.DEBUG
+            true
+        }
+    }
+}
