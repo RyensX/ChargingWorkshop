67511337d7fd9e61696f943cb8c16e2c95b69933
Ryensu <ryensu@qq.com>
2021-04-06 15:35:50 +0800

    [优化]改进文案

--- app/src/main/java/com/su/sharkmanfunc/MainActivity.kt
+++ app/src/main/java/com/su/sharkmanfunc/MainActivity.kt
@@ -2,11 +2,13 @@ package com.su.sharkmanfunc
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Typeface
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.version).apply {
+            val tf = Typeface.createFromAsset(assets, "fonts/bshark_bold.ttf")
+            typeface=tf
+        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             if (!Settings.canDrawOverlays(this)) {
                 val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
