d533386c2f52f7c13a3264155e3b578d134466c4
Ryensu <ryensu@qq.com>
2021-04-08 11:54:53 +0800

    [新增]可选开机自启动

---
+++ app/src/main/java/com/su/sharkmanfunc/BootBroadcastReceiver.kt
@@ -0,0 +1,26 @@
+package com.su.sharkmanfunc
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        intent?.action.also { result ->
+            if (result == Intent.ACTION_BOOT_COMPLETED) {
+                context?.apply {
+                    val sp = getSharedPreferences(
+                        "${context.packageName}_preferences",
+                        Context.MODE_PRIVATE
+                    )
+                    if (sp.getBoolean(getString(R.string.charge_boot_start), false))
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                            startForegroundService(Intent(this, SharkManChargeService::class.java))
+                        else
+                            startService(Intent(this, SharkManChargeService::class.java))
+                }
+            }
+        }
+    }
+}
