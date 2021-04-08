ef84d589f800b475a730bdcf84f380fcbcee41b3
Ryensu <ryensu@qq.com>
2021-04-09 02:07:13 +0800

    [新增]充电电压读取

--- app/src/main/java/com/su/sharkmanfunc/BatteryBroadCastReceiver.kt
+++ app/src/main/java/com/su/sharkmanfunc/BatteryBroadCastReceiver.kt
@@ -20,6 +20,17 @@ class BatteryBroadCastReceiver : BroadcastReceiver() {
             return bc?.let { it.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1) != 0 } ?: false
         }
 
+        /**
+         * 获取电压(mV)
+         */
+        fun getChargeVoltage(context: Context): Int {
+            val bc = context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+            return bc?.getIntExtra(
+                BatteryManager.EXTRA_VOLTAGE,
+                BatteryManager.BATTERY_HEALTH_UNKNOWN
+            ) ?: -1
+        }
+
         fun addBatteryListener(listener: BatteryListener) {
             this.listener = listener
         }
