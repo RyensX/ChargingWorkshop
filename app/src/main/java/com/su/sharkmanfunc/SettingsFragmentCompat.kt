ddfefe1d11ec52ed3df8e3ba97274a5cd00eba58
Ryensu <ryensu@qq.com>
2021-04-24 19:16:49 +0800

    [优化]初始化设置代码样板

--- app/src/main/java/com/su/sharkmanfunc/SettingsFragmentCompat.kt
+++ app/src/main/java/com/su/sharkmanfunc/SettingsFragmentCompat.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
 import androidx.preference.*
 import kotlinx.coroutines.Dispatchers
@@ -30,112 +31,83 @@ class SettingsFragmentCompat : PreferenceFragmentCompat(), Preference.OnPreferen
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.pre_settings, rootKey)
-        //以前台服务方式启动
-        foregroundService = findPreference(getString(R.string.charge_foreground_service))
-        foregroundService?.apply {
-            isEnabled = !SharkManChargeService.isOpen
-            isForegroundService = isChecked
-            onPreferenceClickListener = Preference.OnPreferenceClickListener {
-                if (it is SwitchPreference)
-                    isForegroundService = isChecked
-                true
-            }
-        }
-        //充电守护服务
-        findPreference<SwitchPreference>(getString(R.string.charge_service))?.apply {
-            isChecked = SharkManChargeService.isOpen
-            onPreferenceClickListener =
-                Preference.OnPreferenceClickListener {
-                    if (it is SwitchPreference) {
-                        val intent = Intent(App.globalContext, SharkManChargeService::class.java)
-                        if (it.isChecked) {
-                            if (ChargeAudioManager.INS.checkIsEmptyAudio() || !isEnableAudio)
-                                Toast.makeText(
-                                    App.globalContext,
-                                    "服务已启动，但未开启语音或尚未设置音频(拉到下方设置)，因此无论什么状态都不会发声。",
-                                    Toast.LENGTH_LONG
-                                ).show()
-                            requireActivity().startService(intent)
-                        } else
-                            requireActivity().stopService(intent)
-                        //锁定前台服务设置
-                        foregroundService?.isEnabled = !it.isChecked
-                    }
-                    true
-                }
-        }
-        //是否播放音频
-        findPreference<SwitchPreference>(getString(R.string.charge_is_audio))?.apply {
-            isEnableAudio = isChecked
-            onPreferenceClickListener =
-                Preference.OnPreferenceClickListener {
-                    if (it is SwitchPreference)
-                        isEnableAudio = it.isChecked
-                    true
-                }
-        }
 
-        findPreference<SwitchPreference>(getString(R.string.charge_click_close))?.apply {
-            isClickClose = isChecked
-            onPreferenceClickListener =
-                Preference.OnPreferenceClickListener {
-                    if (it is SwitchPreference)
-                        isClickClose = it.isChecked
-                    true
-                }
-        }
-        //亮屏守护
-        findPreference<SwitchPreference>(getString(R.string.charge_screen_on_open))?.apply {
-            isOpenOnClock = isChecked
-            onPreferenceClickListener =
-                Preference.OnPreferenceClickListener {
-                    if (it is SwitchPreference)
-                        isOpenOnClock = it.isChecked
-                    true
-                }
-        }
-        //守护常亮
-        findPreference<SwitchPreference>(getString(R.string.charge_keep_show))?.apply {
-            isKeepShow = isChecked
-            onPreferenceClickListener =
-                Preference.OnPreferenceClickListener {
-                    if (it is SwitchPreference)
-                        isKeepShow = it.isChecked
-                    true
-                }
-        }
-        //隐藏最近任务
-        findPreference<SwitchPreference>(getString(R.string.is_clear_recent))?.apply {
-            setTaskRecent(requireContext(), isChecked)
-            onPreferenceClickListener =
-                Preference.OnPreferenceClickListener {
-                    if (it is SwitchPreference)
-                        setTaskRecent(requireContext(), isChecked)
-                    true
-                }
-        }
-        //勿扰模式
-        findPreference<SwitchPreference>(getString(R.string.is_not_open_on_full))?.apply {
-            isNotOpenOnFull = isChecked
-            onPreferenceClickListener =
-                Preference.OnPreferenceClickListener {
-                    if (it is SwitchPreference) {
-                        isNotOpenOnFull = it.isChecked
-                        if (!isNotOpenOnFull)
-                            PhoneUtils.removeCheckFullView(requireContext())
-                        else if (SharkManChargeService.isOpen)
-                            PhoneUtils.checkIsOnFullScreen(requireContext())
-                    }
-                    true
-                }
-        }
-
+        initPre()
         initSounds()
     }
 
     private inline fun setTaskRecent(context: Context, isClear: Boolean) {
         val am = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
         am.appTasks?.get(0)?.setExcludeFromRecents(isClear)
+    }
+
+    private inline fun bindPre(
+        @StringRes keyRes: Int,
+        init: SwitchPreference. () -> Unit,
+        crossinline click: SwitchPreference.() -> Unit
+    ): SwitchPreference? {
+        return findPreference<SwitchPreference>(getString(keyRes))?.apply {
+            init()
+            onPreferenceClickListener =
+                Preference.OnPreferenceClickListener {
+                    if (it is SwitchPreference)
+                        click()
+                    true
+                }
+        }
+    }
+
+    private fun initPre() {
+        //以前台服务方式启动
+        foregroundService = bindPre(R.string.charge_foreground_service,
+            { isEnabled = !SharkManChargeService.isOpen }) {
+            isForegroundService = isChecked
+        }
+        //充电守护服务
+        bindPre(R.string.charge_service,
+            { isChecked = SharkManChargeService.isOpen }) {
+            val intent = Intent(App.globalContext, SharkManChargeService::class.java)
+            if (isChecked) {
+                if (ChargeAudioManager.INS.checkIsEmptyAudio() || !isEnableAudio)
+                    Toast.makeText(
+                        App.globalContext,
+                        "服务已启动，但未开启语音或尚未设置音频(拉到下方设置)，因此无论什么状态都不会发声。",
+                        Toast.LENGTH_LONG
+                    ).show()
+                requireActivity().startService(intent)
+            } else
+                requireActivity().stopService(intent)
+            //锁定前台服务设置
+            foregroundService?.isEnabled = !isChecked
+        }
+        //是否播放音频
+        bindPre(R.string.charge_is_audio,
+            { isEnableAudio = isChecked }) {
+            isEnableAudio = isChecked
+        }
+        //亮屏守护
+        bindPre(R.string.charge_screen_on_open, { isOpenOnClock = isChecked }) {
+            isOpenOnClock = isChecked
+        }
+        //守护常亮
+        bindPre(R.string.charge_keep_show,
+            { isKeepShow = isChecked }) {
+            isKeepShow = isChecked
+        }
+        //隐藏最近任务
+        bindPre(R.string.is_clear_recent,
+            { setTaskRecent(requireContext(), isChecked) }) {
+            setTaskRecent(requireContext(), isChecked)
+        }
+        //勿扰模式
+        bindPre(R.string.is_not_open_on_full,
+            { isNotOpenOnFull = isChecked }) {
+            isNotOpenOnFull = isChecked
+            if (!isNotOpenOnFull)
+                PhoneUtils.removeCheckFullView(requireContext())
+            else if (SharkManChargeService.isOpen)
+                PhoneUtils.checkIsOnFullScreen(requireContext())
+        }
     }
 
     private fun initSounds() {
